f051b18794f3ad2928ad0b2dd7a7a1e0503ae95c
ethan <ethan.hopkins101@gmail.com>
2023-10-20 21:27:40 +0100

    container problem

---
+++ Leet_100x_liked/palindrome_number.py
@@ -0,0 +1,33 @@
+'''
+Given an integer x, return true if x is a palindrome, and false otherwise.
+Example 1:
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+----------------------------------------------------------------
+Example 2:
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+-------------------------------------------------------------------
+Example 3:
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+#Approach:
+We start from the latest digit of initial number and build reverted number.
+ Then check is real number equals to reverted and returns the result.
+'''
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+        reversed_number = 0
+        number = x
+        while x > 0:
+            digit = x % 10
+            x = x // 10
+            reversed_number = reversed_number * 10 + digit
+
+        return number == reversed_number
